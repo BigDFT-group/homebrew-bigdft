7f0788dc659b8ee0ede7643c06c8a41a6a610b92
william-dawson <william.dawson@riken.jp>
2025-11-19 16:53:33 +0900

    Add root url

--- Formula/bigdft.rb
+++ Formula/bigdft.rb
@@ -5,6 +5,7 @@ class Bigdft < Formula
   sha256 "99f670d718ee5d3ea83832d76ff3dce38289fb3a9ee9a3284aa90492f019e44e"
 
   bottle do
+    root_url "https://github.com/william-dawson/homebrew-bigdft/releases/download/v1.9.6" 
     rebuild 2
     sha256 arm64_sequoia: "747cf9c55c44109965c9c367ad53333c54c74ea5d9963157ba0d925d74760a8f"
   end
