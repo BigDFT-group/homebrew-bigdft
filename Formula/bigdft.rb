d6231d6cbab905891dd9a52eb15f0ab4f201a91e
william-dawson <william.dawson@riken.jp>
2025-11-19 16:19:25 +0900

    Remove root_url from bigdft.rb bottle
    Removed the root_url from the bottle block.

--- Formula/bigdft.rb
+++ Formula/bigdft.rb
@@ -5,7 +5,6 @@ class Bigdft < Formula
   sha256 "99f670d718ee5d3ea83832d76ff3dce38289fb3a9ee9a3284aa90492f019e44e"
 
   bottle do
-    root_url "https://github.com/william-dawson/homebrew-bigdft/releases/download/v1.9.6"
     rebuild 1
     sha256 arm64_sequoia: "457a06c40169d835ab3203de9a5b2ee504d05961e55900fe1a6b14f846fd2c14"
   end
